39068915a1bfd552298be9587d8ce19f7882cbd3
bytebytebrew <bytebytebrew@qq.com>
2024-08-08 17:53:53 +0800

    🗃️ feat: update token store model

---
+++ backend/prisma/migrations/1_migration/migration.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS "sys_tokens";
+
+-- CreateTable
+CREATE TABLE "sys_tokens" (
+    "id" TEXT NOT NULL,
+    "access_token" TEXT NOT NULL,
+    "refresh_token" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "domain" TEXT NOT NULL,
+    "login_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ip" TEXT NOT NULL,
+    "port" INTEGER,
+    "address" TEXT NOT NULL,
+    "user_agent" TEXT NOT NULL,
+    "request_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" TEXT NOT NULL,
+
+    CONSTRAINT "sys_tokens_pkey" PRIMARY KEY ("id")
+);
