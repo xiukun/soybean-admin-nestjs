5d5dd901f37ea55ac0dfea4a39bcb671c8717e8c
bytebytebrew <bytebytebrew@qq.com>
2024-09-11 17:10:29 +0800

    ⚗️ feat: @Log use

--- backend/src/api/log-audit/operation-log/rest/operation-log.controller.ts
+++ backend/src/api/log-audit/operation-log/rest/operation-log.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { AuthActionVerb, AuthZGuard, UsePermissions } from '@src/infra/casbin';
 import { ApiResponseDoc } from '@src/infra/decorators/api-result.decorator';
+import { Log } from '@src/infra/decorators/log.decorator';
+import { LogInterceptor } from '@src/infra/interceptors/log.interceptor';
 import { ApiRes } from '@src/infra/rest/res.response';
 import {
   OperationLogProperties,
@@ -15,12 +23,18 @@ import { PaginationResult } from '@src/shared/prisma/pagination';
 import { PageOperationLogsQueryDto } from '../dto/page-operation-log.dto';
 
 @UseGuards(AuthZGuard)
+@UseInterceptors(LogInterceptor)
 @ApiTags('Operation Log - Module')
 @Controller('operation-log')
 export class OperationLogController {
   constructor(private queryBus: QueryBus) {}
 
   @Get()
+  @Log('OperationLog', 'Retrieve Paginated Operation Logs', {
+    logParams: true,
+    logBody: true,
+    logResponse: false,
+  })
   @UsePermissions({ resource: 'operation-log', action: AuthActionVerb.READ })
   @ApiOperation({
     summary: 'Retrieve Paginated Operation Logs',
